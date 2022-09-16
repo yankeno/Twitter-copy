94a2b8f952b17b243947221aa44cd4f912eb0fc5
yankeno <kn5sttyryh@gmail.com>
2022-09-16 23:20:24 +0900

    MySQL Workbench お試しちう

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -18,12 +18,29 @@ class DatabaseSeeder extends Seeder
     {
         DB::table('users')->insert([
             [
-              'name'=>'test',
-              'email'=>'test@test.com',
-              'email_verified_at' => now(),
-              'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-              'remember_token' => Str::random(10),
-            ]
-          ]);
+                'name' => 'test',
+                'email' => 'test@test.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+            ],
+            [
+                'name' => 'test2',
+                'email' => 'test2@test.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+            ],
+            [
+                'name' => 'test3',
+                'email' => 'test3@test.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+            ],
+        ]);
     }
 }
--- src/resources/ts/components/pages/Home.tsx
+++ src/resources/ts/components/pages/Home.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import { memo, FC } from "react";
 
-import { ProfileAvatar } from "../atoms/picture/ProfileAvatar";
-import { TweetTextArea } from "../atoms/input/TweetTextArea";
 import { SideInfo } from "../organism/SideInfo";
 import { SideMenu } from "../organism/SideMenu";
-import { TweetArea } from "../molecules/TweetArea";
 import { TimeLine } from "../organism/TimeLine";
 
 export const Home: FC = memo(() => {
