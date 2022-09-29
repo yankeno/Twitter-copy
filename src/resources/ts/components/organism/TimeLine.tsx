6b3e81d511ca00aaf681ede65364cc2fcadc0e76
yankeno <kn5sttyryh@gmail.com>
2022-09-29 23:35:54 +0900

    loading 中の spinner 追加

--- src/resources/ts/components/organism/TimeLine.tsx
+++ src/resources/ts/components/organism/TimeLine.tsx
@@ -1,137 +1,58 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { memo, FC } from "react";
 
 import { PostedTweetArea } from "../molecules/PostedTweetArea";
 import { TweetArea } from "../molecules/TweetArea";
+import { TweetLoadingSpinner } from "../atoms/spinner/TweetLoadingSpinner";
 
 const baseUrl = import.meta.env.VITE_APP_URL;
 
-const async loadTweets = () => {
-    fetch(`${baseUrl}/api/tweet`, {
-        method: "GET",
-    })
-        .then((res) => {
-            if (!res.ok) {
-                alert("ツイートの取得に失敗しました。");
-                return;
-            }
-            return res.json();
-        })
-        .then((data) => {
-            if (data.message !== "successful") {
-                alert("ツイートの取得に失敗しました。");
-                return;
-            }
-            console.log(data.tweets[0].tweet);
-        });
-}
-
 export const TimeLine: FC = memo(() => {
+    const [isLoaded, setIsLoaded] = useState<boolean>(false);
+    const loadTweets = () => {
+        fetch(`${baseUrl}/api/tweet`, {
+            method: "GET",
+        })
+            .then((res) => {
+                if (!res.ok) {
+                    alert("ツイートの取得に失敗しました。");
+                    return;
+                }
+                return res.json();
+            })
+            .then((data) => {
+                if (data.message !== "successful") {
+                    alert("ツイートの取得に失敗しました。");
+                    return;
+                }
+            })
+            .finally(() => {
+                setIsLoaded(true);
+            });
+    };
     useEffect(() => {
-
+        loadTweets();
     }, []);
     return (
-        <div className="w-[40%]">
-            <TweetArea />
-            <PostedTweetArea
-                tweetId={"1"}
-                account="nyanko_star"
-                userName="アンゴラ村長"
-                isAuthAccount={true}
-                avatarUrl="https://pbs.twimg.com/profile_images/1065277773625810946/0kLo6Xb5_x96.jpg"
-                tweet="三助ダサいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいい"
-                isLiked={true}
-            />
-            <PostedTweetArea
-                tweetId={"2"}
-                account="nyanko_star"
-                userName="アンゴラ村長"
-                isAuthAccount={true}
-                avatarUrl="https://pbs.twimg.com/profile_images/1065277773625810946/0kLo6Xb5_x96.jpg"
-                tweet="三助ダサいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいい"
-                isLiked={true}
-            />
-            <PostedTweetArea
-                tweetId={"3"}
-                account="nyanko_star"
-                userName="アンゴラ村長"
-                isAuthAccount={true}
-                avatarUrl="https://pbs.twimg.com/profile_images/1065277773625810946/0kLo6Xb5_x96.jpg"
-                tweet="三助ダサいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいい"
-                isLiked={true}
-            />
-            <PostedTweetArea
-                tweetId={"4"}
-                account="nyanko_star"
-                userName="アンゴラ村長"
-                isAuthAccount={true}
-                avatarUrl="https://pbs.twimg.com/profile_images/1065277773625810946/0kLo6Xb5_x96.jpg"
-                tweet="三助ダサいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいい"
-                isLiked={true}
-            />
-            <PostedTweetArea
-                tweetId={"5"}
-                account="nyanko_star"
-                userName="アンゴラ村長"
-                isAuthAccount={true}
-                avatarUrl="https://pbs.twimg.com/profile_images/1065277773625810946/0kLo6Xb5_x96.jpg"
-                tweet="三助ダサいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいい"
-                isLiked={true}
-            />
-            <PostedTweetArea
-                tweetId={"6"}
-                account="nyanko_star"
-                userName="アンゴラ村長"
-                isAuthAccount={true}
-                avatarUrl="https://pbs.twimg.com/profile_images/1065277773625810946/0kLo6Xb5_x96.jpg"
-                tweet="三助ダサいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいい"
-                isLiked={true}
-            />
-            <PostedTweetArea
-                tweetId={"7"}
-                account="nyanko_star"
-                userName="アンゴラ村長"
-                isAuthAccount={true}
-                avatarUrl="https://pbs.twimg.com/profile_images/1065277773625810946/0kLo6Xb5_x96.jpg"
-                tweet="三助ダサいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいい"
-                isLiked={true}
-            />
-            <PostedTweetArea
-                tweetId={"8"}
-                account="nyanko_star"
-                userName="アンゴラ村長"
-                isAuthAccount={true}
-                avatarUrl="https://pbs.twimg.com/profile_images/1065277773625810946/0kLo6Xb5_x96.jpg"
-                tweet="三助ダサいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいい"
-                isLiked={true}
-            />
-            <PostedTweetArea
-                tweetId={"9"}
-                account="nyanko_star"
-                userName="アンゴラ村長"
-                isAuthAccount={true}
-                avatarUrl="https://pbs.twimg.com/profile_images/1065277773625810946/0kLo6Xb5_x96.jpg"
-                tweet="三助ダサいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいい"
-                isLiked={true}
-            />
-            <PostedTweetArea
-                tweetId={"10"}
-                account="nyanko_star"
-                userName="アンゴラ村長"
-                isAuthAccount={true}
-                avatarUrl="https://pbs.twimg.com/profile_images/1065277773625810946/0kLo6Xb5_x96.jpg"
-                tweet="三助ダサいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいい"
-                isLiked={true}
-            />
-            <PostedTweetArea
-                tweetId={"11"}
-                account="nyanko_star"
-                userName="アンゴラ村長"
-                isAuthAccount={true}
-                avatarUrl="https://pbs.twimg.com/profile_images/1065277773625810946/0kLo6Xb5_x96.jpg"
-                tweet="三助ダサいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいい"
-                isLiked={true}
-            />
-        </div>
+        <>
+            <div className="w-[40%]">
+                <TweetArea />
+                {isLoaded ? (
+                    <PostedTweetArea
+                        tweetId={"1"}
+                        account="nyanko_star"
+                        userName="アンゴラ村長"
+                        isAuthAccount={true}
+                        avatarUrl="https://pbs.twimg.com/profile_images/1065277773625810946/0kLo6Xb5_x96.jpg"
+                        tweet="三助ダサいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいい"
+                        isLiked={true}
+                    />
+                ) : (
+                    <div className="flex justify-center my-2">
+                        <TweetLoadingSpinner />
+                    </div>
+                )}
+            </div>
+        </>
     );
 });
--- src/resources/ts/components/pages/Notification.tsx
+++ src/resources/ts/components/pages/Notification.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { memo, FC } from "react";
+import { TweetLoadingSpinner } from "../atoms/spinner/TweetLoadingSpinner";
 
 export const Notification: FC = memo(() => {
     return (
         <>
             <p>Notification</p>
+            <TweetLoadingSpinner />
         </>
     );
 });
