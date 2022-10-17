0f776a67761a288a5bb148451224babb3ba1f671
yankeno <kn5sttyryh@gmail.com>
2022-10-17 23:48:04 +0900

    sanctum の実装中。postman からのリクエストが上手く通らない。

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Log;
 
 class LoginController extends Controller
 {
